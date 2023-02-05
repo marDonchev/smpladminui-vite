34d3ed3ec8fb302837b71b1cf316b679337adb02
marDonchev <mdonchev@msn.com>
2023-02-05 16:25:07 +0200

    Small update

--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,34 @@
-import { resolve } from 'node:path'
+import { resolve } from "node:path";
 
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
-import dts from 'vite-plugin-dts'
-import EsLint from 'vite-plugin-linter'
-import tsConfigPaths from 'vite-tsconfig-paths'
-const { EsLinter, linterPlugin } = EsLint
-import * as packageJson from './package.json'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+import EsLint from "vite-plugin-linter";
+import tsConfigPaths from "vite-tsconfig-paths";
+const { EsLinter, linterPlugin } = EsLint;
+import * as packageJson from "./package.json";
 // https://vitejs.dev/config/
 export default defineConfig((configEnv) => ({
-  plugins: [
-    dts({
-      include: ['src/components/'],
-    }),
-    react(),
-    tsConfigPaths(),
-    linterPlugin({
-      include: ['./src}/**/*.{ts,tsx}'],
-      linters: [new EsLinter({ configEnv })],
-    }),
-  ],
-  build: {
-    lib: {
-      entry: resolve('src', 'components/index.ts'),
-      name: 'SmplAdminUI',
-      formats: ['es', 'umd'],
-      fileName: (format) => `smpladminui.${format}.js`,
+    plugins: [
+        dts({
+            include: ["src/components/"],
+        }),
+        react(),
+        tsConfigPaths(),
+        linterPlugin({
+            include: ["./src}/**/*.{ts,tsx}"],
+            linters: [new EsLinter({ configEnv })],
+        }),
+    ],
+    build: {
+        lib: {
+            entry: resolve("src", "components/index.ts"),
+            name: "SmplAdminUI",
+            formats: ["es", "umd"],
+            fileName: (format) => `smpladminui.${format}.js`,
+        },
+        rollupOptions: {
+            external: [...Object.keys(packageJson.peerDependencies)],
+        },
     },
-    rollupOptions: {
-      external: [...Object.keys(packageJson.peerDependencies)],
-    },
-  },
-}))
+}));
